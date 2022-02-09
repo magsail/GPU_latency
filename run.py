e251d86928678fbd5e6ab0d5df5cc0fb79c077fb
zhanghao <zhanghaobuaa@gmail.com>
2022-02-08 19:37:50 -0500

    Added Encoder Latency Simulation

--- run.py
+++ run.py
@@ -4,12 +4,17 @@ import matplotlib.pyplot as plt
 # simulation time unit: ms
 
 FPS = 30
-N_BINS = 500
+N_BINS = 200
 FRAME_TIME_MU = 0.35 # unit ms
 FRAME_TIME_SIGMA = 0.1/2.3263  # 99% in +/- 0.1ms
 
-N_FRAME_PER_WAY = 100
+N_FRAME_PER_WAY = 10000
 N_WAYS = 80
+WAY_GAP = 1000 / FPS / 80
+
+WAYS_PER_ENCODER = 10
+N_ENCODER = int(N_WAYS / WAYS_PER_ENCODER)
+LATENCY_FRAME = 1000 / FPS / WAYS_PER_ENCODER
 
 def gen_submission_N(size):
   raw = np.ceil(np.random.uniform(0, 18, size))
@@ -34,12 +39,15 @@ def main():
 
   # Plot the distribution
 
-  """
-  fig, axs = plt.subplots(1, 2)
-  axs[0].hist(frame_time_seq.flatten(), bins=N_BINS)
-  axs[1].hist(frame_submission_N.flatten(), bins=np.linspace(0.5, 10.5, 11), edgecolor='black')
-  plt.show()
-  """
+  fig0 = plt.figure()
+  axs0 = fig0.add_subplot(1, 2, 1)
+  axs0.hist(frame_time_seq.flatten(), bins=N_BINS)
+  axs0.set(xlabel='Latency (ms)', ylabel='Histogram', title='Frame Render Time Distribution')
+  axs0.tick_params(axis='x', which='minor')
+
+  axs1 = fig0.add_subplot(1, 2, 2)
+  axs1.hist(frame_submission_N.flatten(), bins=np.linspace(0.5, 10.5, 11), edgecolor='black')
+  axs1.set(xlabel='No. of Submission', ylabel='Histogram', title='No. of Submission Distribution')
 
   # Gen submission queue
   entire_item_queue = []
@@ -51,7 +59,7 @@ def main():
     way_sub_queue = [] # submission queue of each way
 
     for j in range(N_FRAME_PER_WAY):
-      scheduled_start_time = i * FRAME_TIME_MU + j * 1000 / FPS
+      scheduled_start_time = i * WAY_GAP + j * 1000 / FPS
       sch_time_table[i][j] = scheduled_start_time
 
       item_duration = frame_time_seq[i, j] / frame_submission_N[i, j]
@@ -130,14 +138,74 @@ def main():
 
   print("last sub count: " + str(last_sub_cnt))
 
-  diff_table = complete_time_table - sch_time_table
+  diff_table_GPU = complete_time_table - sch_time_table
 
   # Plot GPU output
-  fig, axs = plt.subplots()
-  axs.hist(diff_table.flatten(), bins=N_BINS)
+  fig1 = plt.figure()
+  axs = fig1.add_subplot(1, 1, 1)
+  #kwargs = dict(histtype='stepfilled', alpha=0.3, density=True, bins=N_BINS)
+  kwargs = dict(alpha=0.7, density=True, bins=N_BINS)
+  l1 = axs.hist(frame_time_seq.flatten(), label='Time w/o blocking', **kwargs)
+  l2 = axs.hist(diff_table_GPU.flatten(), label='Time w/ blocking', **kwargs)
+  axs.tick_params(axis='x', which='minor')
+  axs.set(xlabel='Frame Render Time (ms)', ylabel='Histogram', title='Distribution w/ and w/o blocking')
+  axs.legend(['Time w/o blocking', 'Time w/ blocking'])
+
+  # Add video encoder latency
+  complete_time_codec = np.zeros((N_WAYS, N_FRAME_PER_WAY))
+  current_time_array = np.zeros((N_ENCODER))
+  for i in range(N_FRAME_PER_WAY):
+    for j in range(N_ENCODER):
+      for k in range(WAYS_PER_ENCODER):
+        way_idx = WAYS_PER_ENCODER * j + k
+
+        if i == 0 and k == 0:
+          current_time_array[j] = complete_time_table[way_idx][i] + LATENCY_FRAME
+          complete_time_codec[way_idx][i] = current_time_array[j]
+        else:
+          if complete_time_table[way_idx][i] < current_time_array[j]:
+            current_time_array[j] += LATENCY_FRAME
+            complete_time_codec[way_idx][i] = current_time_array[j]
+          else:
+            current_time_array[j] = complete_time_table[way_idx][i] + LATENCY_FRAME
+            complete_time_codec[way_idx][i] = current_time_array[j]
+
+  diff_table_codec = complete_time_codec - complete_time_table
+
+  # Interleave codec output
+  WAY_GROUP = WAYS_PER_ENCODER
+  complete_time_interleave = np.zeros((N_WAYS, N_FRAME_PER_WAY))
+  current_time_array = np.zeros(N_ENCODER)
+
+  for i in range(N_FRAME_PER_WAY):
+    for j in range(WAY_GROUP):
+      for k in range(N_ENCODER): # 一次做8个，做10次
+        way_idx = j * N_ENCODER + k
+
+        if i == 0 and j == 0: # only first column and first group (8 ways)
+          current_time_array[k] = complete_time_table[way_idx][i] + LATENCY_FRAME
+          complete_time_interleave[way_idx][i] = current_time_array[k]
+        else:
+          if complete_time_table[way_idx][i] < current_time_array[k]:
+            current_time_array[k] += LATENCY_FRAME
+            complete_time_interleave[way_idx][i] = current_time_array[k]
+          else:
+            current_time_array[k] = complete_time_table[way_idx][i] + LATENCY_FRAME
+            complete_time_interleave[way_idx][i] = current_time_array[k]
+
+  diff_table_interleave = complete_time_interleave - complete_time_table
+
+  fig2 = plt.figure()
+  axs0 = fig2.add_subplot(1, 2, 1)
+  axs0.hist(diff_table_codec.flatten(), bins=N_BINS)
+  axs0.set(xlabel='Latency (ms)', ylabel='Histogram', title='Latency Distribution w/o Interleave')
+  axs0.tick_params(axis='x', which='minor')
+
+  axs1 = fig2.add_subplot(1, 2, 2)
+  axs1.hist(diff_table_interleave.flatten(), bins=N_BINS)
+  axs1.set(xlabel='Latency (ms)', ylabel='Histogram', title='Latency Distribution w/ Interleave')
+  axs1.tick_params(axis='x', which='minor')
   plt.show()
-
-
 
 if __name__ == '__main__':
   main()
